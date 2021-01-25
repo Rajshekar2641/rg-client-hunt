f19c8ffd1a40288ab467d43b0be0169b6a112789
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-01-25 11:54:22 -0600

    modified files

---
+++ init-locations.js
@@ -0,0 +1,23 @@
+export default [
+    {
+   Name: "Education building",
+   North: 30.351133,
+   West: -44.882717,
+   South: 30.351049,
+   East: -44.882375,
+   Notes: '',
+   Attachments: '',
+   'Quest-Location': 'Education building',
+ },
+ {
+   Name: 'Administration Building',
+   North: 20.350438,
+   West: -84.883363,
+   South: 20.350296,
+   East: -84.883180,
+   Notes: '',
+   Attachments: '',
+   'Quest-Location': 'Administration Building',
+ },
+
+];
