45a7596d7687d182d9c08ea0a7bd1af66fa7fc20
GANDE RAJASHEKAR <rajshekar2641@gmail.com>
2021-02-05 13:17:16 -0600

    Updated locations

--- init-locations.js
+++ init-locations.js
@@ -1,13 +1,25 @@
 export default [
     {
-        Name: 'Baltimore Avenue',
-        Latitude: 39.0329,
-        Longitude: -94.59
+        Name: '5016 Baltimore Avenue',
+        Latitude: 39.03577746205357,
+        Longitude: -94.58883994100509
     },
     {
         Name: 'Wyandotte Street',
-        Latitude: 39.0331,
-        Longitude:-94.5932
+        Latitude: 39.03553374638238,
+        Longitude:-94.58789161725058
+    },
+    {
+        Name: 'Mission Taco',
+        Latitude:  39.035148041685964,
+        Longitude: -94.58756555809767
+    },
+
+    {
+        Name: 'Andres Main St',
+        Latitude: 39.03626845739615,
+        Longitude: -94.58762656392564 
     }
+
 
 ];
